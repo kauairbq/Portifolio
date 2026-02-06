78e6916c54acc230a3acff67d4e8288f89178dbf
Kauai Rocha <kauai@example.com>
2026-02-06 10:54:02 +0000

    AUTO UPDATE

--- projects/saas-billing/apps/api/src/app.module.ts
+++ projects/saas-billing/apps/api/src/app.module.ts
@@ -1,9 +1,24 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { PrismaModule } from "./prisma/prisma.module";
+import { AuthModule } from "./auth/auth.module";
+import { UsersModule } from "./users/users.module";
+import { PlansModule } from "./plans/plans.module";
+import { SubscriptionsModule } from "./subscriptions/subscriptions.module";
+import { InvoicesModule } from "./invoices/invoices.module";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    AuthModule,
+    UsersModule,
+    PlansModule,
+    SubscriptionsModule,
+    InvoicesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
