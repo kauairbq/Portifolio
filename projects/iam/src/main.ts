97deecacb37efe32d0cf9f031de7a58c03aec54b
Kauai Rocha <kauai@example.com>
2026-02-08 16:24:03 +0000

    AUTO UPDATE

--- projects/iam/src/main.ts
+++ projects/iam/src/main.ts
@@ -1,9 +1,11 @@
-﻿import { NestFactory } from "@nestjs/core";
+import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import * as dotenv from "dotenv";
 
 async function bootstrap() {
+  dotenv.config({ override: true });
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors();
